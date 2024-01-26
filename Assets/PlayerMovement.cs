64fcaf0820d747abc41e003ec4f4e8aa02399a37
Philip Shlifer <ben13ph@gmail.com>
2024-01-26 17:57:47 +0200

    View Point Change

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -9,9 +10,7 @@ public class NewBehaviourScript : MonoBehaviour
     public Rigidbody2D rb;
 
     private Vector2 movement;
-    public Vector2 mousePos;
-
-    public Camera cam;
+    public Vector3 mousePos;
 
 
 
@@ -30,16 +29,23 @@ public class NewBehaviourScript : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 dir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = dir;
+
+        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+
     }
 
     void Move()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-        Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+        //Vector2 lookDir = mousePos - rb.position;
+        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        //rb.rotation = angle;
     }
 
 
