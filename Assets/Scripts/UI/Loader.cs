55ca2c45a14ad1f08851f03a02870856ee886fbe
Philip Shlifer <ben13ph@gmail.com>
2024-01-27 21:32:52 +0200

    MainMenu

---
+++ Assets/Scripts/UI/Loader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        MainMenu,
+        Loader,
+        Level1,
+        Level2
+    }
+
+    public static Scene targetScene;
+
+    public static void load(Scene targetScene)
+    {
+        Loader.targetScene = targetScene;
+
+
+        SceneManager.LoadScene(Scene.Loader.ToString());
+    }
+
+    public static void LoaderCallback()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
